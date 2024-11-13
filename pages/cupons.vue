02628d37291c791ac45033b2a9397edb28dda893
João Gabriel <jgabrieldgg@gmail.com>
2024-11-13 16:00:24 -0300

    new fixs

--- pages/cupons.vue
+++ pages/cupons.vue
@@ -1,70 +1,133 @@
 <script setup>
-const full = ref(false)
+import { ref, onMounted } from 'vue'
+
+const isShareSupported = ref(false)
+
+onMounted(() => {
+  isShareSupported.value = 'share' in navigator
+})
+
+const shareContent = async () => {
+  if (isShareSupported.value) {
+    try {
+      await navigator.share({
+        title: 'Título do Conteúdo',
+        text: 'Descrição do conteúdo que será compartilhado.',
+        url: window.location.href,
+      })
+      console.log('Conteúdo compartilhado com sucesso!')
+    } catch (error) {
+      console.error('Erro ao compartilhar:', error)
+    }
+  } else {
+    alert('O compartilhamento não é suportado neste dispositivo.')
+  }
+}
 </script>
 
 <template>
-    <img class="w-full" src="/img/banner4.png" alt="Selfriends">
-    <div class="bg-white py-8">
+    <img class="w-full" src="/img/banner2.png" alt="Selfriends">
+    <div class="bg-white pt-8 pb-10">
         <Container>
             <div class="flex flex-col items-center">
                 <div class="flex items-center gap-3">
-                    <img class="max-w-[40px] min-w-[40px] h-10 object-cover rounded-full" src="/img/user.png" alt="Cliente Selfit">
-                    <p class="text-[#808080] text-base leading-[100%] font-bold">
+                    <img class="max-w-[40px] min-w-[40px] h-10 object-cover rounded-full" src="/img/user.svg" alt="Cliente Selfit">
+                    <p class="text-black text-base leading-[100%] font-bold">
                         Cliente Selfit
-                        <small class="text-[11px] mt-1 leading-[100%] font-medium block text-[#808080]">cliente@gmail.com</small>
+                        <small class="text-xs mt-1.5 leading-[100%] font-medium block text-[#808080]">cliente@gmail.com</small>
                     </p>
                 </div>
-                <template v-if="full">
-                    <h1 class="font-extrabold text-[27px] text-center uppercase italic leading-[100%] text-black mb-1 mt-6">Parabéns!</h1>
-                    <p class="text-center text-sm font-normal text-black mb-6">Você utilizou todos os cupons disponíveis e ganhou 3 mensalidades gratuitas</p>
-                </template>
-                <template v-else>
-                    <h3 class="mt-6 border-b border-[#e0e0e0] pb-6 mb-6 text-lg leading-[120%] font-medium text-center text-[#5F5F5F]">Esses são os cupons ativos. <br /> Compartilhe e ganhe mais descontos.</h3>
-                    <p class="text-center text-sm font-normal text-black mb-6">Até o momento, você utilizou <strong class="text-black">02</strong> dos seus 12 cupons disponíveis. <br /> Compartilhe com mais amigos para ganhar até <strong class="text-black">3 mensalidades gratuitas</strong></p>
-                </template>
-                <ul class="w-full mb-6 flex flex-col gap-4">
+                <h3 class="w-full mt-6 border-b border-[#e0e0e0] pb-6 mb-6 text-lg leading-[120%] font-medium text-center text-[#5F5F5F]">Esses são os cupons de uso único. <br /> Compartilhe e ganhe mensalidade.</h3>
+                <h4 class="text-center text-black text-xl leading-[110%] italic uppercase font-extrabold mb-2">Você já ganhou <strong class="text-black">X</strong> mensalidades</h4>
+                <p class="text-center text-sm font-normal text-black mb-6">Até o momento, você utilizou <strong class="text-black">02</strong> dos seus 12 cupons disponíveis. <br /> Compartilhe com mais amigos para ganhar até <strong class="text-black">3 mensalidades gratuitas</strong></p>
+                <ul class="w-full mb-8 flex flex-col gap-4">
                     <li
                         v-for="i, index in [
                             {
                                 voucher: 'MGM294UJI298',
-                                status: 'Utilizado',
-                                names: 'Fernanda'
+                                status: 'Disponível',
+                                name: 'Henrique'
                             },
                             {
                                 voucher: 'MGM294UJI298',
-                                status: 'Disponível',
-                                names: 'Henrique'
+                                status: 'Utilizado',
+                                name: 'Fernanda'
+                            },
+                            {
+                                voucher: 'MGM294UJI298',
+                                status: 'Utilizado',
+                                name: 'Tércio'
                             }
                         ]"
                         :key="index"
-                        class="grid grid-cols-2 gap-y-2 pb-4 border-b border-[#F1F1F1]"
+                        class="flex items-center justify-between pb-4 border-b border-[#dbdbdb]"
                     >
-                        <div>
-                            <h6 class="text-center text-[#ACACAC] font-normal text-xs uppercase">Cupom</h6>
-                            <p class="text-center font-bold text-sm text-black">{{ i.voucher }}</p>
+                        <div class="min-w-[120px] max-w-[120px]">
+                            <h6 class="text-[#ACACAC] font-normal text-[11px] uppercase">Cupom</h6>
+                            <p :class="{ '!text-[#ACACAC]': i.status === 'Utilizado' }" class="font-bold text-sm text-black">{{ i.voucher }}</p>
                         </div>
-                        <div>
-                            <h6 class="text-center text-[#ACACAC] font-normal text-xs uppercase">Status</h6>
+                        <div class="min-w-[80px] max-w-[80px]">
+                            <h6 class="text-[#ACACAC] font-normal text-[11px] uppercase">Status</h6>
                             <p
                                 :class="{ '!text-red': i.status === 'Disponível' }"
-                                class="text-center font-bold text-sm text-[#019b01]"
+                                class="font-bold text-sm text-[#019b01]"
                             >
                                 {{ i.status }}
                             </p>
                         </div>
-                        <div>
-                            <h6 class="text-center text-[#ACACAC] font-normal text-xs uppercase">Utilizador por</h6>
-                            <p class="text-center font-bold text-sm text-black">{{ i.names }}</p>
+                        <div
+                            v-if="i.status === 'Utilizado'"
+                            class="min-w-[100px] max-w-[100px] w-full"
+                        >
+                            <h6 class="text-[#ACACAC] font-normal text-[11px] uppercase">Utilizador</h6>
+                            <p class="truncate font-bold text-sm text-black">{{ i.name }}</p>
                         </div>
-                        <a class="flex justify-center items-center gap-2 text-sm uppercase font-bold underline text-black" :href="`https://wa.me/?text=Selfit%20Academias%20%7C%20Tire%20sua%20galera%20do%20sof%C3%A1%0AMeus%20cupons%20foram%20liberados%21%20Corra%20e%20aproveite%20sua%20primeira%20mensalidade%20por%20R%24%2029%2C90%21%20Use%20meu%20cupom%20${i.voucher}%0Ahttps%3A%2F%2Fselfriends.com.br`" target="_blank">
-                            Compartilhe
-                            <img src="/img/whatsapp.svg" alt="WhatsApp">
-                        </a>
+                        <div
+                            v-if="i.status === 'Disponível'"
+                            class="min-w-[100px] max-w-[100px] w-full flex items-center gap-2"
+                        >
+                            <a
+                                :href="`https://wa.me/?text=Selfit%20Academias%20%7C%20Tire%20sua%20galera%20do%20sof%C3%A1%0AMeus%20cupons%20foram%20liberados%21%20Corra%20e%20aproveite%20sua%20primeira%20mensalidade%20por%20R%24%2029%2C90%21%20Use%20meu%20cupom%20${i.voucher}%0Ahttps%3A%2F%2Fselfriends.com.br`"
+                                target="_blank"
+                            >
+                                <img src="/img/whatsapp.svg" alt="WhatsApp">
+                            </a>
+                            <button
+                                v-for="i, index in [
+                                    'instagram',
+                                    'facebook'
+                                ]"
+                                :key="index"
+                                type="button"
+                                @click="shareContent"
+                            >
+                                <img :src="`/img/${i}.svg`" :alt="i">
+                            </button>
+                        </div>
                     </li>
                 </ul>
-                <Button>Compartilhar</Button>
-                <NuxtLink to="/" class="inline-block mt-4 font-bold italic text-base text-black uppercase">Sair</NuxtLink>
-                <NuxtLink href="/Regulamento.pdf" target="_blank" class="text-center mt-6 mb-6 text-xs font-normal leading-[130%] underline text-black">Consulte o regulamento e as condições da campanha clicando aqui.</NuxtLink>
+                <NuxtLink to="/" class="underline inline-block font-bold italic text-base text-black uppercase">Sair</NuxtLink>
+            </div>
+        </Container>
+    </div>
+    <div class="bg-red pt-12">
+        <Container>
+            <div class="flex flex-col items-center">
+                <ul class="flex flex-col gap-3">
+                    <li class="flex items-start gap-2">
+                        <strong class="text-black">1.</strong>
+                        <p class="font-medium text-sm uppercase italic">A cada 4 cupons utilizados <span class="underline">você ganha uma mensalidade</span>.</p>
+                    </li>
+                    <li class="flex items-start gap-2">
+                        <strong class="text-black">2.</strong>
+                        <p class="font-medium text-sm uppercase italic">Cada cupom da acesso a oferta exclusiva do plano plus de <span class="underline">29,90</span> na primeira mensalidade.</p>
+                    </li>
+                    <li class="flex items-start gap-2">
+                        <strong class="text-black">3.</strong>
+                        <p class="font-medium text-sm uppercase italic">Compartilhe os cupons quantas vezes quiser.</p>
+                    </li>
+                </ul>
+                <NuxtLink href="/Regulamento.pdf" target="_blank" class="text-center mt-8 text-xs font-normal leading-[130%]">Consulte o regulamento.</NuxtLink>
             </div>
         </Container>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,27 @@
+<script setup>
+
+</script>
+
 <template>
-    <img class="w-full" src="/img/banner1.png" alt="Chame seus amigos e ganhe benefícios. Tire sua galera do sofá.">
-    <Container>
-        <div class="flex flex-col items-center">
-            <h1 class="text-center uppercase text-lg font-medium italic leading-[130%]">Traga seus amigos <br /> para <strong class="font-bold">Selfit e ganhe até</strong></h1>
-            <h1 class="font-extrabold text-[27px] text-center uppercase italic leading-[100%] mb-4">03 mensalidades <br /> grátis.*</h1>
-            <form class="w-full flex flex-col items-center" action="/chame-seus-amigos" method="POST">
-                <input required class="text-black block w-full h-10 mb-3 bg-white px-4 font-semibold placeholder:font-normal uppercase placeholder:tracking-[2px] text-center placeholder:text-[#999999]" type="email" placeholder="E-mail">
-                <input required class="text-black block w-full h-10 mb-3 bg-white px-4 font-semibold placeholder:font-normal uppercase placeholder:tracking-[2px] text-center placeholder:text-[#999999]" type="password" placeholder="Senha">
-                <Button submit>Partiu desconto</Button>
+    <img class="w-full" src="/img/banner2.png" alt="Ganhe até 03 mensalidades grátis.*">
+    <div class="bg-white pb-12">
+        <Container>
+            <p class="text-black text-center font-medium text-base pt-8 pb-6 leading-[130%]">Acesse utilizando os mesmos dados para entrar na Selfit.</p>
+            <form class="flex flex-col items-center" action="/cupons" method="POST">
+                <input class="block w-full bg-[#f2f2f2] placeholder:opacity-100 placeholder:text-[#999999] placeholder:uppercase placeholder:font-medium font-semibold text-lg text-black text-center mb-3 py-2 placeholder:tracking-[1px] px-4" type="email" placeholder="E-mail" required>
+                <input class="block w-full bg-[#f2f2f2] placeholder:opacity-100 placeholder:text-[#999999] placeholder:uppercase placeholder:font-medium font-semibold text-lg text-black text-center py-2 placeholder:tracking-[1px] px-4" type="password" placeholder="Senha" required>
+                <button class="mt-5 w-full flex items-center gap-6 text-left border-[#e0e0e0] py-6 border-t border-b" type="submit">
+                    <img class="w-8" src="/img/logo-red.svg" alt="Selfit">
+                    <span class="block flex-[2] font-medium text-lg text-black">Entrar com Selfit</span>
+                    <img class="w-4" src="/img/arrow.svg" alt="Seta">
+                </button>
+                <NuxtLink class="underline font-medium text-sm inline-block mt-4 text-black" to="https://admin.selfitacademias.com.br/selfit/0/page/landing-page/login" target="_blank">Esqueci Minha Senha</NuxtLink>
             </form>
-            <NuxtLink class="inline-block mt-6 underline font-medium text-xs" href="https://admin.selfitacademias.com.br/selfit/0/page/landing-page/login" target="_blank">Esqueci Minha Senha</NuxtLink>
-            <p class="mt-10 italic uppercase text-center font-medium text-lg leading-[23px]">Quanto mais <strong>você <span class="underline">indicar</span>,</strong> <br /> mais desconto <strong>pode <span class="underline">ganhar</span>.</strong></p>
-            <NuxtLink href="/Regulamento.pdf" target="_blank" class="text-center mt-4 mb-6 text-xs font-normal leading-[100%] underline">*Para mais informações consulte o regulamento.</NuxtLink>
-        </div>
-    </Container>
+            <p class="text-black mt-16 italic uppercase text-center font-medium text-lg leading-[23px]">Quanto mais <strong class="text-black">você <span class="underline text-black">indicar</span>,</strong> <br /> mais desconto <strong class="text-black">pode <span class="underline text-black">ganhar</span>.</strong></p>
+            <div class="mt-10 flex justify-center">
+                <NuxtLink href="/Regulamento.pdf" target="_blank" class="inline-block text-center text-xs font-normal text-black">Consulte o regulamento.</NuxtLink>
+            </div>
+        </Container>
+    </div>
     <Footer />
 </template>
